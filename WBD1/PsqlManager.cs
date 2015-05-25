2128c93783cdd037ebcf13c2578a6752e131467c
mskwarek <mskwarek@stud.elka.pw.edu.pl>
2015-05-25 19:35:17 +0200

    Fsabnju

---
+++ WBD1/PsqlManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql; 
+namespace WBD1
+{
+    class PsqlManager
+    {
+
+        public PsqlManager()
+        {
+          
+                // Specify connection options and open an connection
+                NpgsqlConnection conn = new NpgsqlConnection("Server=ec2-54-247-79-142.eu-west-1.compute.amazonaws.com;Port=5432;"+
+                            "User Id=scwsyxapuubtqw;Password=H0ROhcTkKnUSyWzox2M5Xc9gi1;Database=df2q62t1b26udq;SSL=true;SslMode=Require;");
+                conn.Open();
+
+                // Define a query
+                NpgsqlCommand cmd = new NpgsqlCommand("select * from filharmonie", conn);
+                Query.nowaFilharmonia(conn, "2", "nazwa", "2015-01-01", "typowy", "typ", "ulica", "5", "1234567890", "123456789");
+
+                // Execute a query
+                NpgsqlDataReader dr = cmd.ExecuteReader();
+
+                // Read all rows and output the first column in each row
+                while (dr.Read())
+                    Console.Write("{0}\n", dr[0]);
+
+                // Close connection
+                conn.Close();
+     
+        }
+    }
+}
---
+++ WBD1/Query.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WBD1
+{
+    public static class Query
+    {
+        public static void nowaFilharmonia(NpgsqlConnection connection, string id, 
+            string nazwa, string data_otwarcia, string imie_dyr, string nazwisko_dyr, 
+            string ulica, string numer_lok, string nip, string regon)
+        {
+            NpgsqlCommand cmd = new NpgsqlCommand("INSERT into Filharmonie(filharmonia_ID, nazwa, data_otwarcia, imie_dyrektora,"+
+                "nazwisko_dyrektora, ulica, numer_lokalu , nip, regon)"+
+                "VALUES ('"+id+"', '" +nazwa+"', '"+imie_dyr+"', '"+nazwisko_dyr+"', '"+ulica+"', '"+numer_lok
+                +"', '"+nip+"', '"+regon+"');", connection);
+        }
+    }
+}
