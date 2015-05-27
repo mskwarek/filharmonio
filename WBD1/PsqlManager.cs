ecfbcd4f47031ac74a99fb8657ec435831dd1509
mskwarek <mskwarek@stud.elka.pw.edu.pl>
2015-05-28 01:41:14 +0200

    nbiufsa

--- WBD1/PsqlManager.cs
+++ WBD1/PsqlManager.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Npgsql; 
+using Npgsql;
+using System.Data; 
 namespace WBD1
 {
     class PsqlManager
@@ -13,23 +14,29 @@ namespace WBD1
         {
           
                 // Specify connection options and open an connection
-                NpgsqlConnection conn = new NpgsqlConnection("Server=ec2-54-247-79-142.eu-west-1.compute.amazonaws.com;Port=5432;"+
-                            "User Id=scwsyxapuubtqw;Password=H0ROhcTkKnUSyWzox2M5Xc9gi1;Database=df2q62t1b26udq;SSL=true;SslMode=Require;");
+                NpgsqlConnection conn = new NpgsqlConnection("Server=89.65.47.175;Port=5432;"+
+                            "User Id=adam;Password= szwagier1;Database=wbd;SSL=true;SslMode=Require;");
                 conn.Open();
 
                 // Define a query
-                NpgsqlCommand cmd = new NpgsqlCommand("select * from filharmonie", conn);
-                Query.nowaFilharmonia(conn, "2", "nazwa", "2015-01-01", "typowy", "typ", "ulica", "5", "1234567890", "123456789");
+               // NpgsqlCommand cmd = new NpgsqlCommand("select * from wbd.poczty", conn);
+                //Query.nowaPoczta(conn, "3", "21422", "Stanin");
+                //Query.nowaFilharmonia(conn, "2", "nazwa", "2015-01-01", "typowy", "typ", "ulica", "5", "1234567890", "123456789");
+                TransactionManager trMen = new TransactionManager();
+                Query.WykonajFunkcjeTrm(conn, "wbd.ins_poczty", trMen,
+                    //new ParametrInfo("poczta_id", DbType.Int32, 3),
+                    new ParametrInfo("kod_pocztowy", DbType.Decimal, 21422),
+                    new ParametrInfo("miejscowosc", DbType.String, "Tuchowicz"));
 
                 // Execute a query
-                NpgsqlDataReader dr = cmd.ExecuteReader();
+                //NpgsqlDataReader dr = cmd.ExecuteReader();
 
                 // Read all rows and output the first column in each row
-                while (dr.Read())
-                    Console.Write("{0}\n", dr[0]);
+                //while (dr.Read())
+                //    Console.Write("{0}\n", dr[0]);
 
                 // Close connection
-                conn.Close();
+                //conn.Close();
      
         }
     }
